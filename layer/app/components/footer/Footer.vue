42ba0730e2f48c009bfb79fb192e82b6917d189d
mhaibaraai <mhaibaraai@gmail.com>
2026-01-07 18:16:22 +0800

    feat: 新增工具函数和 Composables
    - 新增 useTheme/useFaq/useHighlighter composables
    - 新增 unicode 工具函数
    - 新增 Footer 组件目录结构(FooterLeft/FooterRight)
    - 新增 DocsAsideLeftTop/DocsAsideLeftBody 插槽组件

---
+++ layer/app/components/footer/Footer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <USeparator :icon="route.path === '/' ? undefined : 'i-simple-icons-nuxtdotjs'" class="h-px" />
+
+  <UFooter
+    :ui="{
+      left: 'text-sm text-muted',
+      root: 'border-t border-default'
+    }"
+  >
+    <template #left>
+      <FooterLeft />
+    </template>
+
+    <template #right>
+      <FooterRight />
+    </template>
+  </UFooter>
+</template>
---
+++ layer/app/components/footer/FooterLeft.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+const { footer } = useAppConfig()
+</script>
+
+<template>
+  <MDC v-if="footer?.credits" :value="footer.credits" unwrap="p" />
+</template>
