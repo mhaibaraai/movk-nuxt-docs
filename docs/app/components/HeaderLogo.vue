f4da21a8ad932d4ab8aca18f748f50a184beb1a3
mhaibaraai <mhaibaraai@gmail.com>
2026-06-05 15:48:56 +0800

    feat(docs): 新增 HeaderLogo 组件展示站点标识与版本号

---
+++ docs/app/components/HeaderLogo.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const config = useRuntimeConfig().public
+const { header } = useAppConfig()
+</script>
+
+<template>
+  <div class="flex items-center gap-2" aria-label="Site Logo">
+    <NuxtLink :to="header.to">
+      <UUser :avatar="{ src: header.avatar, alt: 'Site Logo', loading: 'lazy' }" :name="header.title" />
+    </NuxtLink>
+
+    <UButton
+      :label="`v${config.version}`"
+      variant="subtle"
+      size="xs"
+      class="font-semibold rounded-full truncate"
+    />
+  </div>
+</template>
